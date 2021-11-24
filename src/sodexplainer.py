85da78b45d040329686b50d8438d5b798f69cbb5
Ishwor Subedi <subedi.esor621@gmail.com>
2021-11-24 11:51:30 +0100

    added the explanation part

--- src/sodexplainer.py
+++ src/sodexplainer.py
@@ -2,6 +2,7 @@ import torch
 import torchvision
 import numpy as np
 
+from lime.lime_image import LimeImageExplainer
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 from src.utils import jaccard
@@ -26,35 +27,41 @@ class SODExplainer:
             self.model.load_state_dict(torch.load(load_from, map_location=torch.device('cpu'))['model_state_dict'])
                    
             
-    def get_class_probability(self, data_obj):
-        """ The Method takes one image from the dataset and returns the class score as the probability 
-            indicating how likely we detect the most confident object in this image.
-        Args:
-            data_obj (img, target): A sample from the PennFudanDataset
-        Returns:
-            probabilities (Tensor): Probability distribution for detecting the most confident object
-        """
-        img, target = data_obj # ground truth
-        
+    def get_class_probability(self,data_obj):
+      _, target = data_obj # ground truth
+      def get_probabilities(img_as_arrays):
         self.model.eval() # set the module in evaluation mode
-        boxes = self.model(img.unsqueeze(0))[0]['boxes']
-        scores = self.model(img.unsqueeze(0))[0]['scores']
-        
-        if len(boxes) == 0: 
-            # if there's no object detected
-            prob = 0
-            print("No object detected!")
-        else:
-            ious = jaccard(target['boxes'],boxes) # ious with shape (num_objs, num_boxes)
-            ious = ious[ious > 0.4].unsqueeze(1) 
-            if len(ious) == 0: # No score above the threshold
-                prob = 0
-            else:
-                obj_idx, box_idx = np.unravel_index(torch.argmax(ious), ious.shape) # retrieve argmax-indices in 2d
-                prob = scores[box_idx] 
-        probabilities = [prob, 1 - prob]      
-        return torch.Tensor(probabilities)
+        img_probs = []
+        for img_as_array in img_as_arrays:
+          img = torch.Tensor(img_as_array)
+          boxes = self.model(img.unsqueeze(0).permute(0,3,1,2))[0]['boxes']
+          scores = self.model(img.unsqueeze(0).permute(0,3,1,2))[0]['scores']
+
+          if boxes.size() == 0: 
+              # if there's no object detected
+              prob = 0
+              print("No object detected!")
+          else:
+              ious = jaccard(target['boxes'],boxes) # ious with shape (num_objs, num_boxes)
+              ious = ious[ious > 0.4].unsqueeze(1) 
+              if ious.size() == 0: # No score above the threshold
+                  prob = 0
+                  print("No score above the threshold!")
+              else:
+                  obj_idx, box_idx = np.unravel_index(torch.argmax(ious), ious.shape) # retrieve argmax-indices in 2d
+                  prob = scores[box_idx] 
+          probabilities = [prob, 1 - prob]
+          img_probs.append(np.array(probabilities))   
+        return np.array(img_probs)
+
+      return get_probabilities
     
-    # def explain(self, data_obj):
-        
-        # Do explanation
+    #get_explnation
+    def get_explanation(self,image_test,data_obj):
+      explainer = LimeImageExplainer(verbose=True)
+      # self.logger.info("Explaining object: ")
+      explanation = explainer.explain_instance(
+          image= image_test,
+          classifier_fn=self.get_class_probability(data_obj),
+          num_samples=10)
+      return explanation
